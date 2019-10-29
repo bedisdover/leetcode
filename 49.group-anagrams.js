820a1b12190084ac6ae4d00821a1f8018408b443
doversong <doversong@tencent.com>
2019-10-29 10:10:03 +0800

    add

---
+++ 49.group-anagrams.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=49 lang=javascript
+ *
+ * [49] Group Anagrams
+ *
+ * https://leetcode.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (49.95%)
+ * Total Accepted:    421.9K
+ * Total Submissions: 839.5K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * Given an array of strings, group anagrams together.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ * ⁠ ["ate","eat","tea"],
+ * ⁠ ["nat","tan"],
+ * ⁠ ["bat"]
+ * ]
+ * 
+ * Note:
+ * 
+ * 
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ * 
+ * 
+ */
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    
+};
